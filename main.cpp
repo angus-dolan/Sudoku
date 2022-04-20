e64a67cbe5a71b72e954f3cf1403a99be589101c
Angus Dolan <angus@angusdolan.com>
2022-04-20 17:12:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <string>
+#include <iostream>
+
+#include "HomeView.h"
 
 using namespace std;
 
 int main()
 {
+	// Declare Views
+	HomeView* homeView = new HomeView();
 
+	//
+	homeView->display();
+
+	system("cls");
+	/*int& choice = homeView->getChoice();*/
+	std::cout << homeView->getChoice() << std::endl;
 }
