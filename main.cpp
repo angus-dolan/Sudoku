f62c928b34c76f5958d9e40a7be7315dfd9566cc
Angus Dolan <angus@angusdolan.com>
2022-04-20 18:17:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 
 #include "ViewRouter.h"
-#include "HomeView.h"
+#include "hideCursor.h"
 
 using namespace std;
 
 int main()
 {
-	ViewRouter* router = new ViewRouter();
+	ViewRouter* router = new ViewRouter(); // Holds all the games views
+	hideCursor(true); // Hide cursor in console
 
 	// Main Menu
 	// 1 = Start Game, 2 = Read Instructions, 3 = Exit Game 
